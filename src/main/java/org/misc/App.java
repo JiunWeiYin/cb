c5a0de78136458537bb4025a60c301650d532c94
ccp <ccp0625@gmail.com>
2017-02-10 23:02:37 +0800

    add annotation

--- src/main/java/org/misc/App.java
+++ src/main/java/org/misc/App.java
@@ -16,62 +16,59 @@ public class App {
 
     public static void main(String[] args) throws IOException {
 
+        // set configuration
         Configuration config = Apps.getConfiguration();
         LOGGER.debug(String.format("The configuration is ready."));
 
+        // setup connection
         Connection conn = Apps.getConnection(config.getUrlBondDaily(), USER_AGENT, REFERRER, TIME_OUT);
         LOGGER.debug(String.format("The connection is ready."));
 
+        // execute connection
         Connection.Response resp = conn.execute();
         LOGGER.debug(String.format("The connection has been tested."));
 
-        if (resp.statusCode() == 200) {
-            Document doc = conn.get();
-            LOGGER.debug(String.format("The HTML has been converted as a Document object."));
-
-
-            Elements tables = doc.select(TABLE); // get all tables
-            if (tables.size() <= 0) {
-                LOGGER.debug(String.format("<%s> was not found.", TABLE));
-                return;
-            }
-            LOGGER.debug(String.format("Got all <%s>.", TABLE));
-
-
-            Element table = Apps.searchTable(tables, CLASS, YUI_TEXT_LEFT);
-            if (table == null) {
-                LOGGER.debug(String.format("<%s %s=%s> was not found.", TABLE, CLASS, YUI_TEXT_LEFT));
-                return;
-            }
-            LOGGER.debug(String.format("Got the <%s %s=%s>.", TABLE, CLASS, YUI_TEXT_LEFT));
-
-
-            table = Apps.searchTable(table.select(TABLE), BGCOLOR, BGCOLOR_VALUE);
-            if (table == null) {
-                LOGGER.debug(String.format("<%s %s=%s> was not found.", TABLE, BGCOLOR, BGCOLOR_VALUE));
-                return;
-            }
-            LOGGER.debug(String.format("Got the <%s %s=%s>.", TABLE, BGCOLOR, BGCOLOR_VALUE));
-
-
-            System.out.printf("table: %s%n", table);
-
-
-//            Elements rows = tables.select("tr");
-//
-//            List<String> downServers = new ArrayList<>();
-//            for (int i = 1; i < rows.size(); i++) { //first row is the col names so skip it.
-//                Element row = rows.get(i);
-//                Elements cols = row.select("td");
-//                if (cols.get(7).text().equals("down")) {
-//                    downServers.add(cols.get(5).text());
-//                }
-//            }
-
-
-        } else {
-            System.out.printf("resp.statusCode(): %s %n", resp.statusCode());
+        // get connection response status code
+        if (resp.statusCode() != 200) {
+            LOGGER.error(String.format("The connection response status code is %s. " +
+                    "Please check if the internet is working.", resp.statusCode()));
+            return;
         }
+        LOGGER.error(String.format("The connection response status code is %s. %n", resp.statusCode()));
+
+        // convert HTML to doc
+        Document doc = conn.get();
+        LOGGER.debug(String.format("The HTML has been converted as a Document object."));
+
+        // select all tables
+        Elements tables = doc.select(TABLE); // get all tables
+        if (tables.size() <= 0) {
+            LOGGER.debug(String.format("<%s> was not found.", TABLE));
+            return;
+        }
+        LOGGER.debug(String.format("Got all <%s>.", TABLE));
+
+        // get a specific table
+        Element table = Apps.searchTable(tables, CLASS, YUI_TEXT_LEFT);
+        if (table == null) {
+            LOGGER.debug(String.format("<%s %s=%s> was not found.", TABLE, CLASS, YUI_TEXT_LEFT));
+            return;
+        }
+        LOGGER.debug(String.format("Got the <%s %s=%s>.", TABLE, CLASS, YUI_TEXT_LEFT));
+
+        // get a specific table
+        table = Apps.searchTable(table.select(TABLE), BGCOLOR, BGCOLOR_VALUE);
+        if (table == null) {
+            LOGGER.debug(String.format("<%s %s=%s> was not found.", TABLE, BGCOLOR, BGCOLOR_VALUE));
+            return;
+        }
+        LOGGER.debug(String.format("Got the <%s %s=%s>.", TABLE, BGCOLOR, BGCOLOR_VALUE));
+
+
+        System.out.printf("table: %s%n", table);
+
+
+
 
     }
 
